7ff20ff0932df85b0b138500036091187d6ea416
dasturchioka <dasturchioka@gmail.com>
2022-08-28 18:46:41 +0500

    initial

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import RegisterView from "../views/RegisterView.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
+    beforeEnter(to, from, next) {
+      window.location.href = "/register";
+    },
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-    }
-  }
-]
+    path: "/register",
+    name: "register",
+    component: RegisterView,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/RegisterView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import logo from "../assets/logo.svg"
+</script>
+
+<template>
+  <div class="register-page h-screen">
+    <div class="container mx-auto sm:px-0 px-4">
+      <div class="register-box h-[90vh]">
+        <div class="box-logo">
+            <img :src="logo" alt="logo testpro gurlan">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.register-page {
+  background: url("../assets/background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: 10% 60%;
+}
+
+.register-box {
+  background: rgba(255, 255, 255, 0.37);
+  box-shadow: 2px 2px 103px -7px rgba(0, 0, 0, 0.04);
+  backdrop-filter: blur(40px);
+  border-radius: 12px;
+}
+</style>
